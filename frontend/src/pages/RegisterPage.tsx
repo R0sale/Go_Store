184f96cc135546e50e32332377c7a20fd06c3c49
Taras Kirsanov <taras.kirsanov@strongpoint.com>
2026-09-14 10:37:18 +0200

    Added the register page on the frontend side

--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -1,15 +1,17 @@
-import type { User } from "../models/user"
-import { newApiService } from "../services/api_service"
 import { useState } from "react"
 import { Link, useNavigate } from "react-router"
+import { newApiService } from "../services/api_service"
+import type { User } from "../models/user"
 import { useUser } from "../stores/user_store"
 
-export const LoginPage = () => {
-    const [isValid, setIsValid] = useState<boolean>(true)
+
+export const RegisterPage = () => {
     const [email, setEmail] = useState<string>("")
     const [password, setPassword] = useState<string>("")
-    const navigate = useNavigate()
+    const [name, setName] = useState<string>("")
+    const [isValid, setIsValid] = useState<boolean>(true)
     const setUser = useUser(state => state.setUser)
+    const navigate = useNavigate()
 
     const inputEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
         setEmail(e.target.value)
@@ -19,16 +21,21 @@ export const LoginPage = () => {
         setPassword(e.target.value)
     }
 
-    const login = async (e: React.SubmitEvent) => {
+    const inputName = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setName(e.target.value)
+    }
+
+    const register = async (e: React.SubmitEvent) => {
         e.preventDefault()
         const apiService = newApiService()
 
         const body = {
             email: email,
-            password: password
+            password: password,
+            name: name
         }
 
-        const { ok, response } = await apiService.apiCall(import.meta.env.VITE_USERS_URL + "/api/users/login", "POST", body)
+        const { ok, response } = await apiService.apiCall(import.meta.env.VITE_USERS_URL + "/register", "POST", body)
 
         if (!ok) {
             setIsValid(false)
@@ -42,22 +49,26 @@ export const LoginPage = () => {
     }
 
     return (
-        <div className="mx-auto my-20 flex w-full max-w-sm flex-col gap-5 rounded-2xl border border-gray-100 bg-white p-8 shadow-xl">
+        <div>
+            <div className="mx-auto my-20 flex w-full max-w-sm flex-col gap-5 rounded-2xl border border-gray-100 bg-white p-8 shadow-xl">
             <h1 className="font-serif text-3xl font-bold text-gray-900">Welcome back</h1>
-            <p className="-mt-3 text-sm text-gray-500">Don't have an account?<Link to={{pathname: "/register"}} className="ml-1 cursor-pointer font-medium text-indigo-600 hover:underline">Sign up free</Link></p>
+            <p className="-mt-3 text-sm text-gray-500">Already have an account?<Link to={{pathname: "/login"}} className="ml-1 cursor-pointer font-medium text-indigo-600 hover:underline">Log In</Link></p>
             <div className="flex cursor-pointer items-center justify-center gap-2 rounded-lg border border-gray-200 bg-gray-50 py-2.5 text-sm font-medium text-gray-700 hover:bg-gray-100">Google</div>
             <div className="relative my-1 text-center text-xs text-gray-400 before:absolute before:top-1/2 before:left-0 before:h-px before:w-[35%] before:bg-gray-200 after:absolute after:top-1/2 after:right-0 after:h-px after:w-[35%] after:bg-gray-200">or email</div>
-            <form onSubmit={login} className="flex flex-col gap-5">
+            <form onSubmit={register} className="flex flex-col gap-5">
                 <label htmlFor="email" className="-mb-3 block text-sm font-medium text-gray-700">Email</label>
                 <input onChange={(e) => inputEmail(e)} name="email" className={`rounded-lg border ${!isValid && "border-red-400 border-2"} border-gray-200 bg-gray-50 px-3 py-2.5 text-sm text-gray-900 placeholder:text-gray-400 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500`} />
                 <label htmlFor="password" className="-mb-3 block text-sm font-medium text-gray-700">Password</label>
                 <input type="password" onChange={(e) => inputPassword(e)} name="password" className={`rounded-lg border ${!isValid && "border-red-400 border-2"} border-gray-200 bg-gray-50 px-3 py-2.5 text-sm text-gray-900 placeholder:text-gray-400 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500`} />
+                <label htmlFor="name" className="-mb-3 block text-sm font-medium text-gray-700">Name</label>
+                <input onChange={(e) => inputName(e)} name="name" className={`rounded-lg border ${!isValid && "border-red-400 border-2"} border-gray-200 bg-gray-50 px-3 py-2.5 text-sm text-gray-900 placeholder:text-gray-400 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500`} />
 
-                {!isValid && <p className="-mb-3 text-red-400 mt-0 pt-0 block text-sm font-medium">Wrong email or password</p>}
+                {!isValid && <p className="-mb-3 text-red-400 mt-0 pt-0 block text-sm font-medium">Wrong creadentials</p>}
 
                 <button type="submit" className="mt-1 rounded-lg bg-indigo-600 py-2.5 text-sm font-semibold text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Sign in</button>
             </form>
             <footer className="text-center text-xs text-gray-400">By signing in you agree to our Terms and Privacy Policy</footer>
+        </div>
         </div>
     )
 }
